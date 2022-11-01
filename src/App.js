f26909502629f9e91ac02d0a9cb05b08082edcb9
Корниенко Сергей Дмитриевич <skornienko@t1-consulting.ru>
2022-11-01 11:26:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { ReactComponent as LogoSqe } from "./logo-sqe.svg";
 import { useSearchParams } from "react-router-dom";
 
 function App() {
-  const { tg, userId } = useTelegram();
+  const { tg } = useTelegram();
 
   const [searchParams] = useSearchParams();
 
@@ -37,10 +37,13 @@ function App() {
           <LogoSqe />
         </div>
         <div>
-          <h1 style={{ color: "white", fontSize: "36px" }}>Карта гостя</h1>
+          <h1
+            style={{ color: "white", fontSize: "36px", marginBottom: "20px" }}
+          >
+            Карта гостя
+          </h1>
         </div>
-        <div>{name}</div>
-        <div>{userId}</div>
+        <div style={{ color: "white", fontSize: "36px" }}>{name}</div>
       </div>
     </div>
   );
