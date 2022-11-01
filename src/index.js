6a568ec421dd0687ceb6f4be81b757cd383f94c6
Корниенко Сергей Дмитриевич <skornienko@t1-consulting.ru>
2022-11-01 11:15:10 +0300

    10

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} path="*" />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
