ece4db43d9b0a266ef3e4726b7974e3d53a5af84
Корниенко Сергей Дмитриевич <skornienko@t1-consulting.ru>
2022-11-01 11:58:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+/* import { Route, Routes, BrowserRouter } from "react-router-dom"; */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <Routes>
         <Route index element={<App />} path="*" />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
+    <App />
   </React.StrictMode>
 );
